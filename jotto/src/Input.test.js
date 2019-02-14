4593c4aefcd958caa2af25b5bed899754ab67eb7
Alex Belke <alex.belke@it-manufactory.com>
2019-02-14 10:57:39 +0100

    quiz completed

--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -37,14 +37,22 @@ describe('render', () => {
     });
   });
   describe('word has been guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    })
     test('renders component without error', () => {
-
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
     });
     test('does not render input box', () => {
-
+      const component = findByTestAttr(wrapper, 'input-box');
+      expect(component.length).toBe(0);
     });
     test('does not render submit button', () => {
-
+      const component = findByTestAttr(wrapper, 'submit-button');
+      expect(component.length).toBe(0);
     });
   });
 });
